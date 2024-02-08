c2b2052442e7022d11558d398c41e797281a9853
RJW <blacksheeprobotics@gmail.com>
2024-02-07 22:43:21 -0500

    created snake class and move()

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import turtle
-from turtle import Screen, Turtle
 import time
+from turtle import Screen
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -8,14 +8,7 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-snake_segments = []
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-for position in starting_positions:
-    snake_limb = turtle.Turtle("square")
-    snake_limb.penup()
-    snake_limb.color("white")
-    snake_limb.goto(position)
-    snake_segments.append(snake_limb)
+snake = Snake()
 screen.update()
 
 game_is_on = True
@@ -24,12 +17,7 @@ while game_is_on and step < 20:
     screen.update()
     time.sleep(0.1)
 
-    for seg_num in range(len(snake_segments)-1, 0, -1):
-        new_x = snake_segments[seg_num - 1].xcor()
-        new_y = snake_segments[seg_num - 1].ycor()
-        snake_segments[seg_num].goto(new_x, new_y)
-    snake_segments[0].forward(10)
-    snake_segments[0].left(90)
+    snake.move()
 
     step += 1
 
