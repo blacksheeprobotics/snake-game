23103855838518f237d199491679ba6f614cfd87
RJW <blacksheeprobotics@gmail.com>
2024-02-08 22:42:12 -0500

    Added food refresh that auto draws a new food in new random location

--- food.py
+++ food.py
@@ -1,6 +1,7 @@
 from turtle import Turtle
 import random
 
+
 class Food(Turtle):
     def __init__(self):
         super().__init__()
@@ -9,6 +10,11 @@ class Food(Turtle):
         self.shapesize(stretch_wid=0.5, stretch_len=0.5)
         self.color("yellow")
         self.speed("fastest")
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
+
+    def refresh(self):
         random_x = random.randint(-280, 280)
         random_y = random.randint(-280, 280)
         self.goto(random_x, random_y)
