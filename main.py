5f17ce9ee663afb2b41be87babc42055512354d2
RJW <blacksheeprobotics@gmail.com>
2024-02-12 00:07:04 -0500

    Added write_score and increase_score methods

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -11,6 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 screen.update()
 
 screen.listen()
@@ -26,9 +28,13 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+    scoreboard.write_score()
 
     # detect collision
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.clear()
+        scoreboard.increase_score()
+        scoreboard.write_score()
 
 screen.exitonclick()
