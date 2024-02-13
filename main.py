5b5951e18faa784cf2d9d69faf80d2ef17050ab4
RJW <blacksheeprobotics@gmail.com>
2024-02-12 23:11:23 -0500

    Added tail collision detection

--- main.py
+++ main.py
@@ -40,5 +40,12 @@ while game_is_on:
         game_is_on = False
         scoreboard.game_over()
 
+    # detect tail collision
+    for segment in snake.snake_segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
